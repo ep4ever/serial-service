4d19ea37588a4733177c00b88232b53063759f22
victor <victor@ep4ever.com>
2023-07-10 20:34:41 +0200

    adding sys qualifier since python 3.7 needs it

--- serialservice.py
+++ serialservice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import yaml
 import argparse
+import sys
 
 from epforever.app import EpforEverApp
 
@@ -21,16 +22,16 @@ def argumentParser(config: dict):
             args.device_name is not None and args.device_port is None or
             args.device_port is not None and args.device_name is None):
         parser.print_help()
-        exit(1)
+        sys.exit(1)
 
     if args.device_name is not None and args.device_port is not None:
         for device in devices:
             if device.get('name') == args.device_name:
                 print("Warning: {} already exists".format(args.device_name))
-                exit(1)
+                sys.exit(1)
             if device.get('port') == args.device_port:
                 print("Warning: {} already exists".format(args.device_port))
-                exit(1)
+                sys.exit(1)
 
         devices.append({
             'name': args.device_name,
@@ -46,12 +47,12 @@ def argumentParser(config: dict):
         if using the command line argument we do not start
         the service in order to be able to add more than one device.
         """
-        exit(0)
+        sys.exit(0)
 
     if len(devices) == 0:
         print("Warning no devices defined!")
         parser.print_help()
-        exit(1)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
@@ -62,7 +63,7 @@ if __name__ == '__main__':
             config = yaml.safe_load(f)
     except FileNotFoundError:
         print("Could not find config.yaml file")
-        exit(1)
+        sys.exit(1)
 
     argumentParser(config=config)
 
