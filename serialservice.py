302e164021357e932c98af5bdafa57d950955618
victor <victor@ep4ever.com>
2023-09-19 18:04:45 +0200

    qa

--- serialservice.py
+++ serialservice.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 from dotenv import dotenv_values
-# import sys
+
 from epforever.app import EpforEverApp
-from epforever.tinydb_adapter import TinyDBAdapter
 from epforever.mariadb_adapter import MariaDBAdapter
 from epforever.sqlitedb_adapter import SqliteDBAdapter
+from epforever.tinydb_adapter import TinyDBAdapter
 
 
 if __name__ == '__main__':
-    dconfig = dotenv_values(".env")
+    dconfig = dotenv_values(dotenv_path=".env")
     mode = dconfig.get('MODE', 'tiny')
     adapter = None
+
     if mode == 'tiny':
         adapter = TinyDBAdapter(dconfig)
     if mode == 'maria':
